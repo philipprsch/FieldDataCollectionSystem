f66afafbcfc41491e697f0d60433ca0a78636af4
Philipp Risch <91801955+philipprsch@users.noreply.github.com>
2024-11-20 13:24:02 +0100

    exception handeling

--- Pico/helpers.py
+++ Pico/helpers.py
@@ -2,19 +2,15 @@ import ujson
 import sys
 import uos
 from machine import Pin, I2C, UART
+from defines import *
+import time
 
 def read_json(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            # Load and parse the JSON content into a Python dictionary
-            data = ujson.load(file)
-            return data
-    except OSError as e:
-        print(f"Error opening file: {e}")
-        return None
-    except ValueError as e:
-        print(f"Error parsing JSON: {e}")
-        return None
+    with open(file_path, 'r') as file:
+        # Load and parse the JSON content into a Python dictionary
+        data = ujson.load(file)
+        return data
+ 
 def is_module_imported(module_name):
     # Check if the module is already in sys.modules
     return module_name in sys.modules
@@ -40,7 +36,16 @@ def does_folder_exist(directory):
         return True  
     except OSError:
         return False  
-    
+
+def ensureFileExistance(file_path): 
+    try:
+        uos.stat(file_path)
+    except OSError as e:
+        if e.args[0] == 2:  # ENOENT error code
+            # File does not exist; create it
+            with open(file_path, "w") as f:
+                f.write("")
+
 def append_to_file(filename, data):
     with open(filename, "a") as file:  # 'a' mode for appending
         file.write(data)
@@ -48,21 +53,20 @@ def append_to_file(filename, data):
         print("DATA appended")
 
 # Read from file
-def read_file(filename):
-    try:
-        with open(filename, "r") as file:
-            content = file.read()
-            print(f"Contents of {filename}:")
-            print(content)
-            return content
-    except OSError:
-        print(f"{filename} does not exist yet.")
-        return ""
+def read_file(filename): #NOTE: Check for OSError
+    with open(filename, "r") as file:
+        content = file.read()
+        #print(f"Contents of {filename}:")
+        #print(content)
+        return content    
 
 #Time functions
 
 def get_readable_time(datetime_tuple):
-    year, month, day, weekday, hours, minutes, seconds = datetime_tuple
+    if len(datetime_tuple) == 7:
+        year, month, day, weekday, hours, minutes, seconds = datetime_tuple
+    elif len(datetime_tuple) == 8:
+        year, month, day, weekday, hours, minutes, seconds, s = datetime_tuple
 
     # Format time
     return f"{day:02d}/{month:02d}/{day:02d} {hours:02d}:{minutes:02d}:{seconds:02d}"
@@ -72,9 +76,15 @@ def get_readable_time(datetime_tuple):
 #Only make one I2C insatnce per bus
 _i2c_instances = {}
 
-def get_i2c_instance(bus=0, scl_pin=None, sda_pin=None, freq=400000):
+class I2CInstancePins(Exception):
+    def __init__(self, bus, scl, sda, scl_new, sda_new):
+        self.message = f"Pins scl={scl_new}, sda={sda_new} do not match existing I2C instance for bus={bus} with scl={scl}, sda={sda}"
+        super().__init__(self.message)
+
+
+def get_i2c_instance(bus, scl_pin=None, sda_pin=None, **kwargs):
     global _i2c_instances
-    
+    if bus is None: raise ValueError("Bus must be specified")
     # Define default pins for each bus (for Raspberry Pi Pico)
     default_pins = {
         0: {'scl': 21, 'sda': 20},  # I2C0 default pins on the Pico (GP21=SCL, GP20=SDA)
@@ -83,8 +93,10 @@ def get_i2c_instance(bus=0, scl_pin=None, sda_pin=None, freq=400000):
     
     # Check if the instance for the requested bus already exists
     if bus in _i2c_instances:
-            #NOTE: Here one could check if an object using the sam bus but other pins has already been initialized
-            return _i2c_instances[bus]
+        #Check if an object using the sam bus but other pins has already been initialized
+        if (scl_pin != _i2c_instances[bus]["scl_pin"] or sda_pin != _i2c_instances[bus]["sda_pin"]):
+            raise I2CInstancePins(bus, _i2c_instances[bus]["scl_pin"], _i2c_instances[bus]["sda_pin"], scl_pin, sda_pin)
+        return _i2c_instances[bus]["obj"]
     
     # Use default pins if none are provided
     if scl_pin is None:
@@ -93,56 +105,57 @@ def get_i2c_instance(bus=0, scl_pin=None, sda_pin=None, freq=400000):
         sda_pin = default_pins[bus]['sda']
     
     # Create a new I2C instance for the bus and store it in the dictionary
-    _i2c_instances[bus] = I2C(bus, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=freq)
+    _i2c_instances[bus] = {
+        "scl_pin": scl_pin,
+        "sda_pin": sda_pin,
+        "obj": I2C(bus, scl=Pin(scl_pin), sda=Pin(sda_pin), **(I2C_DEFAULT_PARAMETERS | kwargs))  # Create a new I2C instance for the bus and store it in the dictionary
+    }
     
-    return _i2c_instances[bus]
+    return _i2c_instances[bus]["obj"]
 
 _UART_instances = {}
 
-UART_BAUDRATE = 9600
-UART_TIMEOUT = 2000
+class UARTInstancePins(Exception):
+    def __init__(self, bus, tx, rx, tx_new, rx_new):
+        self.message = f"Pins tx={tx_new}, rx={rx_new} do not match existing UART instance for bus={bus} with tx={tx}, rx={rx}"
+        super().__init__(self.message)
 
-def get_UART_instance(id, *args, **kwargs): #Kwargs may include Bits, Parity, Stop
-    if (id in _UART_instances):
-        return _UART_instances[id]
+
+def get_UART_instance(bus, tx_pin=None, rx_pin=None, **kwargs): #Kwargs may include Bits, Parity, Stop
+    global _UART_instances
+    if bus is None: raise ValueError("Bus must be specified")
+
+    print(kwargs)
+    print(tx_pin)
+    print(rx_pin)
+
+    if (bus in _UART_instances):
+        #Check if pins are provided and if they maatch pins of current instance
+        if (tx_pin!= _UART_instances[bus]["tx_pin"] or rx_pin!= _UART_instances[bus]["rx_pin"]):
+            raise UARTInstancePins(bus, _UART_instances[bus]["tx_pin"], _UART_instances[bus]["rx_pin"], tx_pin, rx_pin)
+        # Return existing instance
+        return _UART_instances[bus]["obj"]
     default_pins = {
         0: {'tx': 0, 'rx': 1}, 
         1: {'tx': 4, 'rx': 5}
     }
-    _UART_instances[id] = UART(baudrate=UART_BAUDRATE)
-    _UART_instances[id].init(id, **( {"bits":8, "parity":None, "stop":1, "timeout": UART_TIMEOUT} | default_pins[id] | kwargs))
-    return _UART_instances[id]
+    if tx_pin is None:
+        tx_pin = default_pins[bus]['tx']
+    if rx_pin is None:
+        rx_pin = default_pins[bus]['rx']
+    _UART_instances[bus] = {
+        "tx_pin": tx_pin,
+        "rx_pin": rx_pin,
+        "obj": UART(bus, tx=Pin(tx_pin), rx=Pin(rx_pin), **( UART_DEFAULT_PARAMETERS | kwargs))
+    }   
+    return _UART_instances[bus]["obj"]
         
-
-#Exception handeling
-import traceback
-
-# Define a function to log errors to a file
-MAX_LOG_SIZE = 1024 * 1024  # 1 MB
-
-from main import rtc
-
-def log_exception(e, msg=None):
-    try:
-        datetime = get_readable_time(rtc.datetime())
-    except Exception as e2:
-        datetime = "TIME UNKNOWN"
-
-    if (msg is not None): print(msg)
-    print(f"Exception: {str(e)} was logged to file")
-
-    log_file = '/error_log.txt'
+def waitForRespnese(uart, timeout):
+    start_time = time.ticks_ms()
     
-    # Check the size of the log file
-    if uos.stat(log_file)[6] > MAX_LOG_SIZE:
-        uos.rename(log_file, '/error_log_old.txt')  # Archive the old log
-
-    try:
-        with open(log_file, 'a') as f:
-            f.write('--- Exception occurred on {} ---\n'.format(datetime))
-            if msg: f.write(msg + '\n')
-            f.write(str(e) + '\n')
-            f.write(traceback.format_exc() + '\n')
-            f.write('\n')
-    except Exception as log_error:
-        print("Failed to log exception:", log_error)
+    while time.ticks_diff(time.ticks_ms(), start_time) < timeout:
+        if uart.any():  # Check if data is available in the UART buffer
+            return True
+        time.sleep_ms(2)  # Small delay to reduce CPU usage
+    
+    return False
--- Pico/main.py
+++ Pico/main.py
@@ -1,41 +1,42 @@
+
 import uasyncio as asyncio
 import machine
-from machine import RTC, Pin
+from machine import Pin
 import uos  # Use uos instead of os for MicroPython
 import time
-
+from lib.SafeDict import SafeDict
 import lib.sdcard as sdcard
 from lib.ds1307 import DS1307 #External RTC Module
 from lib.task_manager import TaskManager
-
-from helpers import read_json, dynamic_import, append_to_file, read_file, does_folder_exist, get_readable_time, get_i2c_instance, log_exception, get_UART_instance
-
+from defines import *
+from helpers import read_json, dynamic_import, append_to_file, read_file, does_folder_exist, get_readable_time, get_i2c_instance, get_UART_instance
+from lib.Debugger import MyDebugger
+from lib.Communicator import Communicator
+from lib.Config import Config
+import lib.Config as conf
 
 #--------MY IMPORTS-------------------------------------
-from devices.LoggingDevice import LoggingDevice
+from devices.LoggingDevice import LoggingDevice, LoggingDeviceInitFailed
+
+from devices.DHT11_LoggingDevice import *
+from devices.otherLoggingDevices import *
+from devices.Regenmesser_LoggingDevice import *
+from devices.WindDirection_LoggingDeviceS import *
+from devices.WindSpeed_LoggingDeviceS import *
+
+# _ = WindDirection_LoggingDeviceSerial
+# _ = WindSpeed_LoggingDeviceSerial
+
 #from modules.DHT11_LoggingDevice import DHT11_LoggingDevice
 
 
-#Read LoggingDevices file
-logging_devices = read_json("lookup/LoggingDevices.json")
-print(logging_devices)
-
-rtc = RTC() #Use onbaord Pico RTC, can be supplemented by external RTC Module
-
-
-config = read_json("config/config.json")
-if config is None: 
-    e = OSError("No configuration file found")
-    log_exception(e)
-    raise e
-
-
-if (config != None and config["others"]["external_rtc"]):
+if (Config.config.get("others", "external_rtc", fb=False)):
     #Initialize and update onboard RTC module
     rtc_i2c = get_i2c_instance(1, 15, 14, freq=400000)
-    ext_rtc = DS1307(rtc_i2c)
-    ext_rtc.halt(False) # 32 khz crystal enable
-    rtc.datetime(ext_rtc.datetime())
+    conf.rtc = DS1307(rtc_i2c)
+    conf.rtc.halt(False) # 32 khz crystal enable
+    
+print("Datetime is:" + get_readable_time(conf.rtc.datetime()))
 
 spi = machine.SPI(0,
                   baudrate=100000,
@@ -50,45 +51,55 @@ cs = machine.Pin(17, machine.Pin.OUT)
 # Initialize the SD card
 sd = sdcard.SDCard(spi, cs)
 
-
-
 # Mount the SD card onto the file system
 vfs = uos.VfsFat(sd)
-if (not does_folder_exist("/sd")):
-    uos.mount(vfs, "/sd")
+if (not does_folder_exist(PATH_SD_CARD_MOUNT)):
+    uos.mount(vfs, PATH_SD_CARD_MOUNT)
 
-task_manager = TaskManager()
 
-arduino = get_UART_instance(0) #Get Bus 0 Serial Instance to read arduino prints for debugging
 myLoggingDevices = []
-
-#For Arduino Debugging
-arduinoInput = ""
+#time.sleep(1) #Wait for Arduino startup (LED Flashing)
 
 async def main():
-    if config is not None:
-        for device in config["loggingDevices"]:
+    logging_devices = Config.config.get("loggingDevices")
+    print(logging_devices)
+    if logging_devices is not None:
+        for device in logging_devices:
+            if (not device["alias"]): raise ValueError("Device does not feature 'alias' attribute in configuration file")
+            if (not device["id"]): raise ValueError(f"Device {device['alias']} does not feature 'id' attribute in configuration file")
+            if (not device["constructor_args"]): raise ValueError(f"Device {device['alias']} does not feature 'constructor_args' attribute in configuration file")
             #print(device)
-            myLoggingDevices.append(LoggingDevice.create_instance(device["id"], **(device["constructor_args"] | {'alias': device["alias"]})))
+            try:
+                new_instance = LoggingDevice.create_instance(device["id"], **(Config.lookup_ld.get(device["id"], "constructor_arg_defaults", fb={}) | device["constructor_args"] | {'alias': device["alias"]}))
+            except LoggingDeviceInitFailed as e:
+                MyDebugger.logException(e)
+            else:
+                myLoggingDevices.append(new_instance)
+
+    #Print all devices set up on the Pico
+    MyDebugger.log("Listing Local Devices:")
+    for device in myLoggingDevices:
+        MyDebugger.log(f"Logging Device: {device.alias}, Type: {device.__class__.__name__}, ID: {device.id} Interval: {device.interval}")
+        
+
+    #Print all Devices setup on each controller
+    controllers = Config.config.get("controllers")
+    if controllers is not None:
+        for alias in controllers.keys():
+            com = Communicator(alias)
+            MyDebugger.log(f"Conroller {alias} Device List: \n {com.list()}")
+    
+    MyDebugger.log(await TaskManager.list_tasks())
 
     while True:
-        #await task_manager.list_tasks()
-        #Listen to Arduino
-        if arduino.any():
-            res = arduino.read()
-            if res.decode() == "\n":
-                print("Ard: "+arduinoInput)
-                arduinoInput = ""
-            else:
-                arduinoInput += res.decode()
-
-        await asyncio.sleep(1)
+        await asyncio.sleep(10)
 
 # Run the event loop
 try:
     asyncio.run(main())
 except Exception as e: #Fatal exception
-    log_exception(e, rtc.datetime())
+    raise #TODO: Just temporary, remove after testing
+    #MyDebugger.logException(e)
     error_led = Pin("LED", Pin.OUT)
     for i in range(0, 5):
         error_led.on()
